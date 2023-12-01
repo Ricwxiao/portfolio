2b48d9e41c2717b0f9fd3fe62cd13ec1e8efe963
Ricwxiao <cxiao4@uw.edu>
2023-12-01 00:23:31 -0800

    barebone design for home page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let cursor = document.querySelector('.cursor');
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.right = e.pageY + 'px';
-});
+// let cursor = document.querySelector('.cursor');
+// document.addEventListener('mousemove', (e) => {
+//     cursor.style.left = e.pageX + 'px';
+//     cursor.style.right = e.pageY + 'px';
+// });
